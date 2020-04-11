f18329409dabfc3b647a3d0cc0b0ae45168b6b47
Till Beemelmanns <till.beemelmanns@rwth-aachen.de>
2020-04-11 11:58:55 +0200

    update

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ from scipy.optimize import curve_fit
 
 CSV_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/' \
           'csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-COUNTRIES = ["Germany",
-             "US",
-             "China"]
 
+COUNTRIES = [
+    {"country": "Germany", "min_infections": 15, "duration_exponential_phase": 45},
+    {"country": "US", "min_infections": 15, "duration_exponential_phase": 45},
+]
 
 def exponential(x, a, k, b):
     return a*np.exp(x*k) + b
+
+
+def sigmoidal(x, a, k, b):
+    return a*(1.0 / (1+np.exp(-(x-k)))) + b
 
 
 def get_csv():
@@ -26,7 +31,7 @@ def get_csv():
     return df
 
 
-def plot_exponential(df, country):
+def plot_exponential(df, country, min_infections, duration_exponential_phase):
 
     # filter just one country
     df = df[df["Country/Region"] == country]
@@ -35,25 +40,55 @@ def plot_exponential(df, country):
     df = df.iloc[0]  # convert to pd.Series
 
     # start with first infections
-    df = df[df.values != 0]
+    df = df[df.values > min_infections]
 
     # parse to datetime
     df.index = pd.to_datetime(df.index, format='%m/%d/%y')
 
     # fit to exponential function
-    time_in_days = np.arange(len(df.values))
+    time_in_days = np.arange(duration_exponential_phase)
     poptimal_exponential, pcovariance_exponential = curve_fit(
-        exponential, time_in_days, df.values, p0=[0.3, 0.205, 0]
+        exponential, time_in_days, df.values[:duration_exponential_phase], p0=[1, 0.35, 0]
     )
 
-    # Plot current DATA
+    # Compute prediction
+    prediction_in_days = 10
+    time_in_days_extra = np.arange(
+        start=0, stop=duration_exponential_phase+prediction_in_days
+    )
+    prediction = exponential(time_in_days_extra, *poptimal_exponential).astype(int)
+    df_prediction = pd.Series(prediction)
+
+    # convert index to dates
+    df_prediction.index = pd.date_range(
+        start=df.index[0],
+        periods=duration_exponential_phase + prediction_in_days,
+        closed="left"
+    )
+
     fig, ax = plt.subplots(figsize=(15, 10))
-    ax.plot(df.index, df.values, '*', label="Infections in Germany")
+    # plot real data
     ax.plot(
-         df.index,
+        df.index,
+        df.values,
+        '*',
+        color="blue",
+        markersize=5,
+        label=f"Infections in {country}")
+    # plot exponential phase
+    ax.plot(
+         df.index[:duration_exponential_phase],
          exponential(time_in_days, *poptimal_exponential),
          'g-',
-         label="Exponential Fit"
+         linewidth=2,
+         label="Exponential Phase"
+    )
+    # plot prediction
+    ax.plot(
+        df_prediction.index[duration_exponential_phase:],
+        df_prediction.values[duration_exponential_phase:],
+        'r--',
+        label="Predicted Number of Infections"
     )
     ax.set_xlabel("Date")
     ax.set_ylabel("Number of Infections")
@@ -64,40 +99,6 @@ def plot_exponential(df, country):
     fig.autofmt_xdate()
     fig.savefig(f"plots/exponential_fit_{country}.png", bbox_inches='tight')
 
-    # Compute prediction
-    prediction_in_days = 10
-    time_in_days = np.arange(
-        start=len(df.values), stop=len(df.values)+prediction_in_days
-    )
-    prediction = exponential(time_in_days, *poptimal_exponential).astype(int)
-    df_prediction = pd.Series(prediction)
-
-    # convert index to dates
-    df_prediction.index = pd.date_range(
-        df.index[-1], periods=prediction_in_days + 1, closed="right"
-    )
-
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(df_prediction)
-
-    # Plot prediction
-    fig, ax = plt.subplots(figsize=(15, 10))
-    ax.plot(df.index, df.values, '*', label=f"Infections in {country}")
-    ax.plot(
-        df_prediction.index,
-        df_prediction.values,
-        'r--',
-        label="Predicted Number of Infections"
-    )
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Number of Infections")
-    ax.legend()
-    ax.grid()
-    ax.xaxis.set_major_locator(mdates.DayLocator(interval=2))
-    fig.suptitle(f"{date.today()} - Number of Infected persons in {country} - 10 day Extrapolation")
-    fig.autofmt_xdate()
-    fig.savefig(f"plots/exponential_extrapolation_{country}.png", bbox_inches='tight')
-
 
 if __name__ == '__main__':
 
@@ -106,11 +107,8 @@ if __name__ == '__main__':
 
     df = get_csv()
 
-    for country in COUNTRIES:
-
-        try:
-            plot_exponential(df, country)
-        except:
-            print(f"Could not plot exponential curve for Country {country}")
-
-
+    for country_info in COUNTRIES:
+        plot_exponential(df,
+                         country_info["country"],
+                         country_info["min_infections"],
+                         country_info["duration_exponential_phase"])
