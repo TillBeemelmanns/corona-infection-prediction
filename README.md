1deac62ec531f73768546db56b238e43fc162c14
Till Beemelmanns <till.beemelmanns@rwth-aachen.de>
2020-06-03 15:26:40 +0200

    Add linear phase

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Live Covid-19 extrapolation of infected persons in Germany
 
 ### Current situation - Germany
-![](plots/exponential_fit_Germany.png)
+![](plots/model_Germany.png)
 
 ### Current situation - US
-![](plots/exponential_fit_US.png)
+![](plots/model_US.png)
 
 ## Data source
 [Data Repository by Johns Hopkins CSSE](https://github.com/CSSEGISandData/COVID-19)
--- main.py
+++ main.py
@@ -13,9 +13,15 @@ CSV_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/' \
           'csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
 COUNTRIES = [
-    {"country": "Germany", "min_infections": 15, "duration_exponential_phase": 45},
-    {"country": "US", "min_infections": 15, "duration_exponential_phase": 45},
+    {"country": "Germany", "min_infections": 15,
+     "start_exponential_phase": 0, "end_exponential_phase": 44,
+     "start_linear_phase": 44, "end_linear_phase": 60},
+
+    {"country": "US", "min_infections": 15,
+     "start_exponential_phase": 0, "end_exponential_phase": 42,
+     "start_linear_phase": 42, "end_linear_phase": 75},
 ]
+
 
 def exponential(x, a, k, b):
     return a*np.exp(x*k) + b
@@ -25,13 +31,19 @@ def sigmoidal(x, a, k, b):
     return a*(1.0 / (1+np.exp(-(x-k)))) + b
 
 
+def linear_func(x, a, b):
+    return a*x + b
+
+
 def get_csv():
     print("Downloading New Data")
     df = pd.read_csv(CSV_URL)
     return df
 
 
-def plot_exponential(df, country, min_infections, duration_exponential_phase):
+def plot_model(df, country, min_infections,
+               start_exponential_phase, end_exponential_phase,
+               start_linear_phase, end_linear_phase):
 
     # filter just one country
     df = df[df["Country/Region"] == country]
@@ -46,25 +58,49 @@ def plot_exponential(df, country, min_infections, duration_exponential_phase):
     df.index = pd.to_datetime(df.index, format='%m/%d/%y')
 
     # fit to exponential function
-    time_in_days = np.arange(duration_exponential_phase)
+    duration_exponential_phase = end_exponential_phase - start_exponential_phase
+    days_exponential_phase = np.arange(duration_exponential_phase) + start_exponential_phase
     poptimal_exponential, pcovariance_exponential = curve_fit(
-        exponential, time_in_days, df.values[:duration_exponential_phase], p0=[1, 0.35, 0]
+        exponential, days_exponential_phase, df.values[start_exponential_phase:end_exponential_phase], p0=[1, 0.35, 0]
     )
 
-    # Compute prediction
+    # compute exponential prediction
     prediction_in_days = 10
     time_in_days_extra = np.arange(
-        start=0, stop=duration_exponential_phase+prediction_in_days
+        start=start_exponential_phase, stop=duration_exponential_phase+prediction_in_days
     )
     prediction = exponential(time_in_days_extra, *poptimal_exponential).astype(int)
-    df_prediction = pd.Series(prediction)
+    df_prediction_exponential = pd.Series(prediction)
 
     # convert index to dates
-    df_prediction.index = pd.date_range(
-        start=df.index[0],
+    df_prediction_exponential.index = pd.date_range(
+        start=df.index[start_exponential_phase],
         periods=duration_exponential_phase + prediction_in_days,
         closed="left"
     )
+
+    # fit to linear function
+    duration_linear_phase = end_linear_phase - start_linear_phase
+    days_linear_phase = np.arange(duration_linear_phase) + start_linear_phase
+    poptimal_linear, pcovariance_linear = curve_fit(
+        linear_func, days_linear_phase, df.values[start_linear_phase:end_linear_phase], p0=[1, 1]
+    )
+
+    # compute linear prediction
+    prediction_in_days = 20
+    time_in_days_extra = np.arange(
+        start=start_linear_phase, stop=end_linear_phase+prediction_in_days
+    )
+    prediction_linear = linear_func(time_in_days_extra, *poptimal_linear).astype(int)
+    df_prediction_linear = pd.Series(prediction_linear)
+
+    # convert index to dates
+    df_prediction_linear.index = pd.date_range(
+        start=df.index[start_linear_phase],
+        periods=duration_linear_phase + prediction_in_days,
+        closed="left"
+    )
+
 
     fig, ax = plt.subplots(figsize=(15, 10))
     # plot real data
@@ -77,18 +113,33 @@ def plot_exponential(df, country, min_infections, duration_exponential_phase):
         label=f"Infections in {country}")
     # plot exponential phase
     ax.plot(
-         df.index[:duration_exponential_phase],
-         exponential(time_in_days, *poptimal_exponential),
+         df.index[start_exponential_phase:end_exponential_phase],
+         exponential(days_exponential_phase, *poptimal_exponential),
          'g-',
          linewidth=2,
          label="Exponential Phase"
     )
-    # plot prediction
+    # plot exponential prediction
     ax.plot(
-        df_prediction.index[duration_exponential_phase:],
-        df_prediction.values[duration_exponential_phase:],
+        df_prediction_exponential.index[duration_exponential_phase:],
+        df_prediction_exponential.values[duration_exponential_phase:],
         'r--',
         label="Exponential Phase Prediction"
+    )
+    # plot linear phase
+    ax.plot(
+        df.index[start_linear_phase:end_linear_phase],
+        linear_func(days_linear_phase, *poptimal_linear),
+        'm-',
+        linewidth=2,
+        label="Linear Phase"
+    )
+    # plot linear prediction
+    ax.plot(
+        df_prediction_linear.index[duration_linear_phase:],
+        df_prediction_linear.values[duration_linear_phase:],
+        'm--',
+        label="Linear Phase Prediction"
     )
     ax.set_xlabel("Date")
     ax.set_ylabel("Number of Infections")
@@ -97,7 +148,7 @@ def plot_exponential(df, country, min_infections, duration_exponential_phase):
     ax.xaxis.set_major_locator(mdates.DayLocator(interval=2))
     fig.suptitle(f"{date.today()} - Number of Infected persons in {country}")
     fig.autofmt_xdate()
-    fig.savefig(f"plots/exponential_fit_{country}.png", bbox_inches='tight')
+    fig.savefig(f"plots/model_{country}.png", bbox_inches='tight')
 
 
 if __name__ == '__main__':
@@ -108,7 +159,10 @@ if __name__ == '__main__':
     df = get_csv()
 
     for country_info in COUNTRIES:
-        plot_exponential(df,
-                         country_info["country"],
-                         country_info["min_infections"],
-                         country_info["duration_exponential_phase"])
+        plot_model(df,
+                   country_info["country"],
+                   country_info["min_infections"],
+                   country_info["start_exponential_phase"],
+                   country_info["end_exponential_phase"],
+                   country_info["start_linear_phase"],
+                   country_info["end_linear_phase"])
